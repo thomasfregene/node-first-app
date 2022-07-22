47fd4ef29eebd2188df7ae194e8f62e2c6af68bd
ThomasFregene <fregzythomas@gmail.com>
2022-07-22 16:08:01 +0100

    Objects

--- javascript-basics/basic.js
+++ javascript-basics/basic.js
@@ -1,22 +1,10 @@
-//primitive datatypes
-const name = 'Max';
-let age = 20;
-const hasHobbies = true;
-age = 30;
-//console.log(age);
+const person ={
+    name: 'John',
+    age: 29,
+    greet(){
+        console.log('Hi, I am ' + this.name);
+    }
+};
 
-//arrow function
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobby: ' + userHasHobby;
-}
-
-//arrow function with one return
-const add = (a,b)=>a+b;
-const addOne = a=>a+1; 
-const addRandom = ()=>1+2;
-
-var result = summarizeUser(name, age, hasHobbies);
-
-var sum = addOne(1);
-var sum = addRandom();
-console.log(sum);
+person.greet();
+//console.log(person)
