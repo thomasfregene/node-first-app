46fdb488c8cf58f553b647fc7321c8dc27c95457
ThomasFregene <fregzythomas@gmail.com>
2022-07-22 15:40:59 +0100

    arrow function

--- javascript-basics/basic.js
+++ javascript-basics/basic.js
@@ -1,13 +1,22 @@
 //primitive datatypes
-var name = 'Max';
-var age = 20;
-var hasHobbies = true;
-//console.log(hasHobbies);
+const name = 'Max';
+let age = 20;
+const hasHobbies = true;
+age = 30;
+//console.log(age);
 
-//function
-function summarizeUser(userName, userAge, userHasHobby){
+//arrow function
+const summarizeUser = (userName, userAge, userHasHobby) => {
     return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobby: ' + userHasHobby;
 }
 
+//arrow function with one return
+const add = (a,b)=>a+b;
+const addOne = a=>a+1; 
+const addRandom = ()=>1+2;
+
 var result = summarizeUser(name, age, hasHobbies);
-console.log(result);
+
+var sum = addOne(1);
+var sum = addRandom();
+console.log(sum);
