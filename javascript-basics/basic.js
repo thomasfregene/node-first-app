3ea6961ff6147be130696282b008830f8517d829
ThomasFregene <fregzythomas@gmail.com>
2022-07-25 08:38:25 +0100

    Destructuring

--- javascript-basics/basic.js
+++ javascript-basics/basic.js
@@ -6,7 +6,17 @@ const person ={
     }
 };
 
+const printName = ({name})=>{
+    console.log(name)
+}
+
+printName(person);
+
+const {name, age} = person;
+
 const hobbies = ['Sports', 'Cooking', 'Reading'];
+const [hobby1, hobby2, hobby3] = hobbies;
+console.log(hobby1, hobby2, hobby3);
 // for(let hobby of hobbies){
 //     console.log(hobby);
 // }
@@ -17,22 +27,22 @@ const hobbies = ['Sports', 'Cooking', 'Reading'];
 
 // const copiedArray = hobbies.slice();
 /*Spread Operator*/
-const copiedArray = [...hobbies];
+// const copiedArray = [...hobbies];
 
-console.log(copiedArray);
+// console.log(copiedArray);
 
-const copiedPerson = {...person};
-//console.log(copiedPerson.greet());
+// const copiedPerson = {...person};
+// //console.log(copiedPerson.greet());
 
-/*Rest Operator*/
-// const toArray = (arg1, arg2, arg3)=>{
-//     return [arg1, arg2, arg3];
+// /*Rest Operator*/
+// // const toArray = (arg1, arg2, arg3)=>{
+// //     return [arg1, arg2, arg3];
+// // }
+
+// // console.log(toArray(1,2,3,4))
+
+// const toArray = (...args)=>{
+//     return args;
 // }
 
-// console.log(toArray(1,2,3,4))
-
-const toArray = (...args)=>{
-    return args;
-}
-
-console.log(toArray(1,2,3,4))
+// console.log(toArray(1,2,3,4));
