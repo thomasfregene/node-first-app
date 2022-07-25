c042fe4805d78251f031dbea7c19377e43140a7c
ThomasFregene <fregzythomas@gmail.com>
2022-07-25 09:16:12 +0100

    Async Code and Promise

--- javascript-basics/basic.js
+++ javascript-basics/basic.js
@@ -1,48 +1,23 @@
-const person ={
-    name: 'John',
-    age: 29,
-    greet(){
-        console.log('Hi, I am ' + this.name);
-    }
-};
-
-const printName = ({name})=>{
-    console.log(name)
+const fetchData =()=>{
+    const promise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve('Done!')
+        }, 1500);
+    });
+    return promise
 }
 
-printName(person);
 
-const {name, age} = person;
+setTimeout(()=>{
+    console.log('Timer is done!');
+    fetchData().then(text=>{
+        console.log(text);
+        //nexting promise
+        return fetchData();
+    }).then(text2=>{
+        console.log(text2);
+    })
+},2000)
 
-const hobbies = ['Sports', 'Cooking', 'Reading'];
-const [hobby1, hobby2, hobby3] = hobbies;
-console.log(hobby1, hobby2, hobby3);
-// for(let hobby of hobbies){
-//     console.log(hobby);
-// }
-
-// console.log(hobbies.map(hobby=> 'Hobby: ' + hobby));
-// hobbies.pop('Baking');
-// console.log(hobbies);
-
-// const copiedArray = hobbies.slice();
-/*Spread Operator*/
-// const copiedArray = [...hobbies];
-
-// console.log(copiedArray);
-
-// const copiedPerson = {...person};
-// //console.log(copiedPerson.greet());
-
-// /*Rest Operator*/
-// // const toArray = (arg1, arg2, arg3)=>{
-// //     return [arg1, arg2, arg3];
-// // }
-
-// // console.log(toArray(1,2,3,4))
-
-// const toArray = (...args)=>{
-//     return args;
-// }
-
-// console.log(toArray(1,2,3,4));
+console.log('Hello');
+console.log('Hi');
