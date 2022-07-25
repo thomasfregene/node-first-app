ff9ffcb0abe9788a7975630eb0f5b597d096b768
ThomasFregene <fregzythomas@gmail.com>
2022-07-25 10:24:39 +0100

    sending response

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method, req.headers);
     //process.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title>My First page</title></header>');
+    res.write('<body><h1>hello from Node.js Sever!</h1></body>');
+    res.write('</html>');
+    res.end
 });
 
 server.listen(3000);
