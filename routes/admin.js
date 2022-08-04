1a5d4479cf5440178693cdbebb0ee2dea6a3cb85
ThomasFregene <fregzythomas@gmail.com>
2022-08-04 17:15:04 +0100

    using helper function for navigation(rootDir)

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,13 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
 router.use(bodyParser.urlencoded({extended: false}));
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../','views','add-product.html'))
+    res.sendFile(path.join(rootDir,'views','add-product.html'))
 });
 
 // /admin/add-product => POST
