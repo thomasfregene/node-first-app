2f68a782f266f5e48396e41df9ae8cb412a96743
ThomasFregene <fregzythomas@gmail.com>
2022-08-04 15:27:49 +0100

    filtering routes path

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
+
+app.use('/admin', adminRoutes);
+app.use('/shop',shopRoutes);
+
+
 app.use((req, res, next)=>{
     res.status(404).send('<h1>Page not found</h1>')
 })
-
-app.use(adminRoutes);
-app.use(shopRoutes);
-
-
-
 
 
 
