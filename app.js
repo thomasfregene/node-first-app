d10ed9ed869ca804a85432fd85eac3b8eea7d586
ThomasFregene <fregzythomas@gmail.com>
2022-08-04 16:13:59 +0100

    creating and serving html pages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
+
 const express = require('express');
 
+const path = require('path');
 
 
 const app = express();
@@ -11,11 +13,11 @@ const shopRoutes = require('./routes/shop');
 
 
 app.use('/admin', adminRoutes);
-app.use('/shop',shopRoutes);
+app.use('/',shopRoutes);
 
 
 app.use((req, res, next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, './','views','page-not-found.html'));
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
@@ -8,13 +9,13 @@ router.use(bodyParser.urlencoded({extended: false}));
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../','views','add-product.html'))
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next)=>{
     console.log(req.body);
-    res.redirect('/shop');
+    res.redirect('/');
 })
 
 module.exports = router;
