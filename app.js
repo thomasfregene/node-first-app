90c490027caf50c3ff98f4fb5b9ea3f153939775
ThomasFregene <fregzythomas@gmail.com>
2022-08-04 15:13:00 +0100

    using expressjs router

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 
 const express = require('express');
-const bodyParser = require('body-parser')
+
+
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 
-app.use('/', (req, res, next)=>{
-    next();
-});
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
 
-app.use('/', (req, res, next)=>{
-    res.send('<h1>Hello from Expression!</h1>');
-});
+
+
+
+
+
 
 
 app.listen(3000);
