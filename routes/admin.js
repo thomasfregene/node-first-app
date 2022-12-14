dec88ccebf69ae89038c9c82188e4c4cb88a26e0
ThomasFregene <fregzythomas@gmail.com>
2022-12-14 16:44:56 +0100

    edit product module

--- routes/admin.js
+++ routes/admin.js
@@ -17,6 +17,9 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct)
 
